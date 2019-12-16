b408e47b6d79efed7b0a9450bb1148a37d3aa3ba
Macky <49353641+MackyReborn@users.noreply.github.com>
2019-12-16 19:38:45 +0000

    Added Check all Bans Function
    Now checks all bans and posts their profile link below instead of listing and writing a number for the requested profile.

--- Source/main.py
+++ Source/main.py
@@ -3,7 +3,7 @@ import functions
 functions.readProfiles()
 
 print("Welcome to the Cheater Tracker Written by Macky in Python\n")
-print("Please Select: \n1)Add Profile\n2)Remove Profile\n3)List Profiles\n4)Exit\n")
+print("Please Select: \n1)Add Profile\n2)Remove Profile\n3)List Profiles\n4)Check Bans for All\n5)Exit\n")
 choice = int(input())
 
 if choice == 1:
@@ -18,16 +18,22 @@ elif choice == 3:
     functions.listProfiles()
     profileEntry = int(input("\n"))
     
-    x = int(input("Please Select: \n1)Open Profile\n2)Check Ban Status\n"))
+    x = int(input("\nPlease Select: \n1)Open Profile\n2)Check Ban Status\n"))
 
     if x == 1:
         functions.openProfile(functions.getProfileEntry(profileEntry))
     elif x == 2:
+        print("\n")
         functions.checkBan(functions.getProfileEntry(profileEntry))
     else:
         print("Invalid Input. . .")
 
 elif choice == 4:
+    for x in range(len(functions.profiles)):
+        functions.checkBan(functions.getProfileEntry(x))
+        print(functions.getProfileEntry(x), "\n")
+
+elif choice == 5:
     exit(0)
 
 else:
