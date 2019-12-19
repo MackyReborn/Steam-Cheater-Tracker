b9ad0c8a8906fd1974a897ad006363362901ac66
Macky <49353641+MackyReborn@users.noreply.github.com>
2019-12-19 14:32:18 +0000

    New Features + Bug Fixes
    *Fixed remove profile function
    *Fixed smaller bugs
    
    *Added config system
    *Added ability to autorun the list all profiles
    *Added ability to remove account on ban check if it returns true
    *Added exception handling for stability
    
    By default, autorun is off and autodelete profile is on, but you can change these easily in the json file.

--- Source/functions.py
+++ Source/functions.py
@@ -1,12 +1,52 @@
 import webbrowser
 import os
+import sys
+import json
 import time
 import string
 import array
+import valve
 import requests
 from bs4 import BeautifulSoup
 
 profiles = []
+
+def readSettings():
+    input_string = open("settings.json", "r")
+    return json.load(input_string)
+
+
+def getAlias(url):
+    y = url.replace("\n", "")
+    res = requests.get(y)
+    steam_page = res.content
+    soup = BeautifulSoup(steam_page, 'html.parser')
+    persona = soup.find('span', class_='actual_persona_name')
+
+    stringpersona = str(persona)
+
+    alias = stringpersona.replace('<span class="actual_persona_name">', '')
+    alias = alias.replace('</span>', '')
+
+    return alias
+
+
+def addUser(url):
+    return True
+
+'''
+def getPermenantURL(url):
+    if url.find('profiles') > -1:
+        return url
+    y = url.replace('\n', '')
+    rurl = "https://steamid.xyz/" + y
+
+    res = requests.get(rurl)
+    page = res.content
+    soup = BeautifulSoup(page, 'html.parser')
+
+    iurl = soup.findAll('input')
+'''
 
 def readProfiles():
     theFile = open("profiles.txt", "r")
@@ -24,15 +64,20 @@ def openProfile(url):
 
 def listProfiles():
     for entry in profiles:
-        print(entry)
+        entry.replace('\n', '')
+        print(entry, getAlias(entry), '\n')
 
 
 def addProfile(profile):
     profiles.append(profile)
 
 
-def removeProfile(profile):
-    profiles.remove(profile)
+def removeProfile(prof):
+    profile = prof + '\n'
+    try:
+        profiles.remove(profile)
+    except:
+        print("Error Removing Selected Profile. ")
 
 
 def saveProfiles():
@@ -73,7 +118,10 @@ def checkBan(url):
 
     if output.find("game ban on record") > -1:
         print("Account is Overwatch Banned")
+        return True
     elif output.find("VAC ban on record") > -1:
         print("Account is VAC Banned")
+        return True
     else:
         print("Account is not Banned")
+        return False
--- Source/main.py
+++ Source/main.py
@@ -1,22 +1,31 @@
 import functions
+import sys
+import config
 
 functions.readProfiles()
+config = config.init()
+
 
 print("Welcome to the Cheater Tracker Written by Macky in Python\n")
 print("Please Select: \n1)Add Profile\n2)Remove Profile\n3)List Profiles\n4)Check Bans for All\n5)Exit\n")
-choice = int(input())
+
+choice = None
+if config[1] == True:
+    choice = 4
+else:
+    choice = int(input())
 
 if choice == 1:
     profile = input("Enter Profile URL to Add: ")
     functions.addProfile(profile)
 
 elif choice == 2:
-    profile = input("Enter Profile URL to Remove: ")
-    functions.removeProfile(profile)
+    prof = input("Enter Profile URL to Remove: ")
+    functions.removeProfile(prof)
 
 elif choice == 3:
     functions.listProfiles()
-    profileEntry = int(input("\n"))
+    profileEntry = int(input("\n")) - 1
     
     x = int(input("\nPlease Select: \n1)Open Profile\n2)Check Ban Status\n"))
 
@@ -29,9 +38,19 @@ elif choice == 3:
         print("Invalid Input. . .")
 
 elif choice == 4:
+    print('\n')
     for x in range(len(functions.profiles)):
-        functions.checkBan(functions.getProfileEntry(x))
-        print(functions.getProfileEntry(x), "\n")
+        profileEntryx = str(functions.getProfileEntry(x))
+        alias = functions.getAlias(profileEntryx)
+        try:
+            print(alias)
+        except:
+            non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+            alias = alias.translate(non_bmp_map)
+            print(alias)
+        if functions.checkBan(profileEntryx) and config[0] == True: #If the setting for this is true in the config
+            functions.removeProfile(profileEntryx)
+        print('\n')
 
 elif choice == 5:
     exit(0)
