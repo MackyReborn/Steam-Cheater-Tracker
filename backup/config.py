d65eec15aebd0cb5a49858be61a75243f3189493
Macky <49353641+MackyReborn@users.noreply.github.com>
2019-12-20 20:18:19 +0000

    Removed useless files

--- backup/config.py
+++
@@ -1,9 +0,0 @@
-import json
-import functions
-
-def init():
-    settings = functions.readSettings()
-    autodelete = settings["autodelete"]
-    autorun = settings["autorun"]
-
-    return [autodelete, autorun]
